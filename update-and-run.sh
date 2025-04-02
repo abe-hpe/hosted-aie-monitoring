9fa19e287eb48fc02144702a0ce39d476bd35e53
Abe Sharp <abe@hpe.com>
2025-04-02 16:18:52 -0700

    Update update-and-run.sh

--- update-and-run.sh
+++ update-and-run.sh
@@ -2,5 +2,6 @@
 
 curl https://raw.githubusercontent.com/abe-hpe/hosted-aie-monitoring/refs/heads/main/aiemon.sh > ~/.aiemon/aiemon.sh
 curl https://raw.githubusercontent.com/abe-hpe/hosted-aie-monitoring/refs/heads/main/mail.json > ~/.aiemon/mail.json
+curl https://raw.githubusercontent.com/abe-hpe/hosted-aie-monitoring/refs/heads/main/slack.json > ~/.aiemon/slack.json
 chmod a+x ~/.aiemon/aiemon.sh
 ~/.aiemon/aiemon.sh
